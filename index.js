bd1ebc7e042e177c15a308fc1fe330f89f806c5e
Will Ley <wley3337@gmail.com>
2018-07-14 14:35:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your code in this file!
+function scuberGreetingForFeet(distance){
+ if (distance <= 400){
+   return 'This one is on me!';
+ } else if (distance > 2000 && distance < 2500){
+   return `I will gladly take your thirty bucks.`;
+ } else if (distance >2500){
+   return `No can do.`;
+   
+ }
+}
+
+function ternaryCheckCity(city){
+  return (city == 'NYC' ? `Ok, sounds good.` : `No go.`);
+}
+
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.';
+      
+    case 'not as generous':
+        return 'Thank you.';
+       
+    default:
+        return `Bye.`;
+  }
+}
+
