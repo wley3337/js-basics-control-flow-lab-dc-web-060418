ecfc40850fa1d1e1a6b27a49bf241da0d55e83ab
Will Ley <wley3337@gmail.com>
2018-07-14 14:13:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function scuberGreetingForFeet(distance){
  if (distance <= 400){
    return 'This one is on me!';
- } else if (distance > 2000 && distance > 2500){
+ } else if (distance > 2000 && distance < 2500){
    return `I will gladly take your thirty bucks.`;
  } else if (distance >2500){
    return `No can do!`;
